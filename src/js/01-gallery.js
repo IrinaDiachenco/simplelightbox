9e391d8efb7d396f8784611c68a8931319bbbf0a
IrinaDiachenco <bilairina@ukr.net>
2023-07-25 17:44:06 +0300

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItem = document.querySelector('.gallery');
+const galleryItemMarkup = createGalleryItemsMarkup(galleryItems);
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(image => {
+       return `
+<li class="gallery__item">
+        <a
+            class="gallery__link"
+            href="${image.original}"
+        >
+            <img
+                class="gallery__image"
+                src="${image.preview}"
+                data-source="${image.original}"
+                alt="${image.description}"
+            />
+        </a>
+    </li>`;
+    
+    }).join('');
+};
+
+galleryItem.insertAdjacentHTML('beforeend', galleryItemMarkup);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    overlay: true,
+    overlayOpacity: 0.7,
+    caption: true,
+   captionsData: 'alt',
+     captionDelay: 250,});
+
