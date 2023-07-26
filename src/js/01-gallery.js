482ddb455ec22aa3369ecfd47306eaf6806aaaf4
IrinaDiachenco <bilairina@ukr.net>
2023-07-26 16:49:42 +0300

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,33 +6,27 @@ console.log(galleryItems);
 
 const galleryItem = document.querySelector('.gallery');
 const galleryItemMarkup = createGalleryItemsMarkup(galleryItems);
+galleryItem.insertAdjacentHTML('beforeend', galleryItemMarkup);
+
 function createGalleryItemsMarkup(galleryItems) {
     return galleryItems.map(image => {
        return `
-<li class="gallery__item">
+
         <a
-            class="gallery__link"
+            class="gallery__item"
             href="${image.original}"
         >
             <img
                 class="gallery__image"
                 src="${image.preview}"
-                data-source="${image.original}"
+                
                 alt="${image.description}"
             />
         </a>
-    </li>`;
-    
+    `
     }).join('');
 };
 
-galleryItem.insertAdjacentHTML('beforeend', galleryItemMarkup);
+const lightbox = new SimpleLightbox('.gallery a', {});
 
-
-const lightbox = new SimpleLightbox('.gallery a', {
-    overlay: true,
-    overlayOpacity: 0.7,
-    caption: true,
-   captionsData: 'alt',
-     captionDelay: 250,});
 
